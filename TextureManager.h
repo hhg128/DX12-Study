b85d63d3dd60f4da3fb959a2d7a85783b49803b1
BHS <BHS@DESKTOP-O8UR9N3>
2017-02-23 01:39:05 +0900

    피곤해서 일단 LoadImageDataFromFile 함수를 인터넷에서 복붙했음, 나중에 분석하면서 자신의 것으로 만들자.

--- TextureManager.h
+++ TextureManager.h
@@ -8,6 +8,13 @@ public:
 
 	void Initialize();
 
+	void	LoadTextureFromFile(std::wstring fileName);
+	int		LoadImageDataFromFile(BYTE** imageData, D3D12_RESOURCE_DESC& resourceDescription, LPCWSTR filename, int &bytesPerRow);
+
+	int		GetDXGIFormatBitsPerPixel(DXGI_FORMAT& dxgiFormat);
+	DXGI_FORMAT GetDXGIFormatFromWICFormat(WICPixelFormatGUID& wicFormatGUID);
+	WICPixelFormatGUID GetConvertToWICFormat(WICPixelFormatGUID& wicFormatGUID);
+
 	std::unordered_map<std::wstring, std::unique_ptr<Texture>>		m_TextureMap;
 };
 
