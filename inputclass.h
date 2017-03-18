74bb511392f4b972893e40e384aa02bd5bc26512
BHS <BHS@DESKTOP-O8UR9N3>
2017-03-18 14:28:50 +0900

    direct Input 준비중
    Todo. Window Height, Width 가져올 수 있는 함수 만들기

--- inputclass.h
+++ inputclass.h
@@ -1,9 +1,14 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Filename: inputclass.h
 ////////////////////////////////////////////////////////////////////////////////
-#ifndef _INPUTCLASS_H_
-#define _INPUTCLASS_H_
+#pragma once
 
+#define DIRECTINPUT_VERSION 0x0800
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <dinput.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Class name: InputClass
@@ -24,8 +29,17 @@ public:
 
 private:
 	bool m_keys[256];
+
+	// Direct Input
+	IDirectInput8* m_directInput;
+	IDirectInputDevice8* m_keyboard;
+	IDirectInputDevice8* m_mouse;
+
+	unsigned char m_keyboardState[256];
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth, m_screenHeight;
+	int m_mouseX, m_mouseY;
 };
 
 extern InputClass* gInput;
-
-#endif
